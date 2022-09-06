625d5e1a83ad7873422c3f5de454bb22f57b4f6b
Clément ROGER <clemmrogg@gmail.com>
2022-09-06 17:48:03 +0200

    added an animated loading logo while we wait for the API answer

---
+++ frontend/src/components/Loader/Loader.tsx
@@ -0,0 +1,3 @@
+import LoaderLogo from "./loader.svg"
+
+export const Loader = () => <img height={40} src={LoaderLogo} alt="Loading Logo" />
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Pokemon } from "../../components/Pokemon/Pokemon"
 import styles from "./Home.module.css"
+import { Loader } from "../../components/Loader/Loader"
 
 interface Pokemon {
   name: string
@@ -17,26 +18,35 @@ const fetchPokemons = async () => {
 
 export const Home = () => {
   const [pokemonList, updatePokemonList] = React.useState<Pokemon[]>([])
+  const [isLoading, setIsLoading] = React.useState(true)
 
   React.useEffect(() => {
-    fetchPokemons().then(pokemons => updatePokemonList(pokemons))
+    fetchPokemons().then(pokemons => {
+      updatePokemonList(pokemons)
+      setIsLoading(false)
+    })
   }, [])
 
   return (
     <div className={styles.intro}>
       <h1>Pokedex</h1>
       <div className={styles.pokemonCards}>
-        {pokemonList.map(pokemon => {
-          return (
-            <Pokemon
-              name={pokemon.name}
-              id={pokemon.id}
-              key={pokemon.id}
-              weight={pokemon.weight}
-              height={pokemon.height}
-            />
-          )
-        })}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          pokemonList.map(pokemon => {
+            return (
+              <Pokemon
+                name={pokemon.name}
+                id={pokemon.id}
+                key={pokemon.id}
+                weight={pokemon.weight}
+                height={pokemon.height}
+              />
+            )
+          })
+        )}
+        {}
       </div>
     </div>
   )
